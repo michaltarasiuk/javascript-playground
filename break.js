cb87a3025a6a8da951c0776a5858195cdf02ffea
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-03-28 23:10:27 +0100

    add 'break'

---
+++ break.js
@@ -0,0 +1,8 @@
+outerBlock: {
+  {
+    console.log('1');
+    break outerBlock; // breaks out of both innerBlock and outerBlock
+    // console.log(':-('); // skipped
+  }
+  // console.log('2'); // skipped
+}
